a2c792e191abb83edb57ee6ec7c17b7fc6b1b78b
Alexander Ott <66271487+AlexanderHott@users.noreply.github.com>
2024-02-08 14:47:30 -0500

    update schema

--- schema.sql
+++ schema.sql
@@ -6,7 +6,6 @@ CREATE TABLE IF NOT EXISTS motion_picture (
 	production VARCHAR(255) NOT NULL,
 	release_date DATETIME NOT NULL,
 	budget FLOAT NOT NULL,
-	-- ? ratings Rating[]
 	-- awards Award[]
 	-- shooting_locations ShootingLocation[]
   rating FLOAT NOT NULL,
@@ -19,20 +18,20 @@ CREATE TABLE IF NOT EXISTS motion_picture (
 CREATE TABLE IF NOT EXISTS movie (
 	id INTEGER PRIMARY KEY NOT NULL,
 	box_office_collection FLOAT,
-  FOREIGN KEY(id) REFERENCES motion_picture(id)
+  FOREIGN KEY(id) REFERENCES motion_picture(id) ON DELETE CASCADE
 );
 
 -- Entity: TvSeries
 CREATE TABLE IF NOT EXISTS tv_series (
 	id INTEGER PRIMARY KEY NOT NULL,
 	no_seasons INTEGER,
-  FOREIGN KEY(id) REFERENCES motion_picture(id)
+  FOREIGN KEY(id) REFERENCES motion_picture(id) ON DELETE CASCADE
 );
 
 -- Entity: Genre
 CREATE TABLE IF NOT EXISTS genre (
-	id INTEGER PRIMARY KEY NOT NULL,
-	name VARCHAR(255) NOT NULL,
+	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL,
+	name VARCHAR(32) NOT NULL,
 	description VARCHAR(255) NOT NULL,
 	-- motion_pictures MotionPicture[]
 	-- CONSTRAINT UniqueGenre UNIQUE (name, description)
@@ -45,8 +44,8 @@ CREATE TABLE IF NOT EXISTS motion_picture_genre_association (
 	genre_id INTEGER NOT NULL,
 
 	PRIMARY KEY (motion_picture_id, genre_id),
-  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id),
-  FOREIGN KEY(genre_id) REFERENCES genre(id)
+  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id) ON DELETE CASCADE,
+  FOREIGN KEY(genre_id) REFERENCES genre(id) ON DELETE CASCADE
 )
 
 -- Entity: Person
@@ -68,22 +67,9 @@ CREATE TABLE IF NOT EXISTS role (
 	role VARCHAR(255) NOT NULL,-- should be own table?
 
 	PRIMARY KEY (person_id, motion_picture_id, role),
-  FOREIGN KEY(person_id) REFERENCES person(id),
-  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id)
+  FOREIGN KEY(person_id) REFERENCES person(id) ON DELETE CASCADE,
+  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id) ON DELETE CASCADE
 );
-
--- CREATE TABLE IF NOT EXISTS rating (
--- 	user_id INTEGER AUTO_INCREMENT NOT NULL,
--- 	motion_picture_id INTEGER NOT NULL,
--- 	val INTEGER NOT NULL,	
---
--- 	PRIMARY KEY (person_id, motion_picture_id),
---   FOREIGN KEY(user_id) REFERENCES user(id),
---   FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id)
---
--- 	ADD CONSTRAINT ValueBetween0And10
--- 	CHECK ( val >= 0 AND val <= 10 )
--- )
 
 -- Entity: Award
 CREATE TABLE IF NOT EXISTS award (
@@ -94,8 +80,8 @@ CREATE TABLE IF NOT EXISTS award (
 
   -- only one award per person per movie
 	PRIMARY KEY (person_id, motion_picture_id, name),
-  FOREIGN KEY(person_id) REFERENCES person(id),
-  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id)
+  FOREIGN KEY(person_id) REFERENCES person(id) ON DELETE CASCADE,
+  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id) ON DELETE CASCADE
 );
 
 -- Entity: ShootingLocation
@@ -105,7 +91,7 @@ CREATE TABLE IF NOT EXISTS shooting_location (
 	city VARCHAR(255) NOT NULL,
 	country VARCHAR(255) NOT NULL,
 
-  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id),
+  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id) ON DELETE CASCADE,
 	PRIMARY KEY (
 		motion_picture_id, name, city, country
 	)
@@ -113,10 +99,13 @@ CREATE TABLE IF NOT EXISTS shooting_location (
 
 -- Entity: User
 CREATE TABLE IF NOT EXISTS user (
-	id INTEGER PRIMARY KEY NOT NULL
-	email VARCHAR(255) NOT NULL UNIQUE
+	id INTEGER AUTO_INCREMENT PRIMARY KEY NOT NULL
+	email VARCHAR(255) NOT NULL
 	name VARCHAR(255) NOT NULL,
-	age INTEGER NOT NULL -- positive
+	age INTEGER NOT NULL, -- positive
+
+  UNIQUE(email),
+  CHECK (age >= 0)
 );
 
 CREATE TABLE IF NOT EXISTS motion_picture_like (
@@ -124,11 +113,10 @@ CREATE TABLE IF NOT EXISTS motion_picture_like (
 	motion_picture_id INTEGER NOT NULL,
 	PRIMARY KEY (user_id, motion_picture_id),
 
-  FOREIGN KEY(user_id) REFERENCES user(id),
-  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id)
+  FOREIGN KEY(user_id) REFERENCES user(id) ON DELETE CASCADE,
+  FOREIGN KEY(motion_picture_id) REFERENCES motion_picture(id) ON DELETE CASCADE
 );
 
--- TODO: Indexes
--- TODO: Views
-
--- is rating a table or just a number (users rate motion_pictures)
+-- covering and overlap constraints in SQL?
+-- genre description as TEXT?
+-- names consistent between schema and diagram
